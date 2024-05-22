ff0c02d35dcb64b733475fc5f25fac2f418978f7
resqiar <resqiagengr@gmail.com>
2024-05-23 03:28:37 +0700

    fix: set app info package name as unique

--- migrations/000003_create_app_info_table.up.sql
+++ migrations/000003_create_app_info_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS app_info (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT NOT NULL,
-    package_name TEXT NOT NULL,
+    package_name TEXT UNIQUE NOT NULL,
     lock_status BOOLEAN DEFAULT false,
     icon TEXT,
     time_usage BIGINT default 0,
